48c365f15fdce027409c66fa08e24d1200f45e07
javenkn <javenkn7@yahoo.com>
2016-07-16 10:48:16 -1000

    got the scss files to compile to the css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,43 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+var PathTo = {
+  SassFiles: './scss/**/*.scss',
+  PublicFolder: './public',
+  PublicCss: './public/css',
+  PublicCssFiles: './public/css/*.css'
+};
 
-  browserSync.init({
-      server: "./public"
-  });
-
-  gulp.watch("scss/**/*.scss", ['sass']);
-  gulp.watch("public/*.html").on('change', browserSync.reload);
+gulp.task('watch-files', function (){
+  gulp.watch(PathTo.SassFiles, ['compile-sass']);
+  // gulp.watch(PathTo.PublicCssFiles, ['html']);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src("scss/styles.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("public/css"))
-    .pipe(browserSync.stream());
+gulp.task('compile-sass', function (){
+  return gulp
+          .src(PathTo.SassFiles, ['compile-sass'])
+          .pipe(sass().on('error', sass.logError))
+          .pipe(gulp.dest(PathTo.PublicCss));
 });
 
-gulp.task('default', ['serve']);
+// gulp.task('html', function (){
+//   return gulp.src('./public/index.html')
+//     .pipe(connect.reload());
+// });
+
+// gulp.task('public-server', function (){
+//   connect.server({
+//     root: './public',
+//     port: 8282,
+//     livereload: true
+//   });
+//   browserSync.init({
+//     server: "./public",
+//     livereload: true
+//   });
+//     gulp.watch("public/*.html").on('change', browserSync.reload);
+// });
+
+gulp.task('default', ['compile-sass', 'watch-files']);
