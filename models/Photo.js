341989ab534f3be349c44eed7ca7ea3f73f4dafb
javenkn <javenkn7@yahoo.com>
2016-07-27 21:28:54 -1000

    recreated the Photos table using migrations

--- models/Photo.js
+++ models/Photo.js
@@ -1,9 +1,15 @@
+'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Photo = sequelize.define("Photo", {
+  var Photo = sequelize.define('Photo', {
     url: DataTypes.TEXT,
     author: DataTypes.STRING,
     description: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
   });
-
   return Photo;
 };
