518a40c773526085c53a2b0ca5a07fce7a85691f
javenkn <javenkn7@yahoo.com>
2016-07-30 19:18:47 -1000

    added user model and migration

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     username: DataTypes.STRING,
-    password: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
