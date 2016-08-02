1754e243f451db44261ce90ca284c4855669a714
javenkn <javenkn7@yahoo.com>
2016-08-02 02:25:45 -1000

    added a config file to put the passport in

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+module.exports = function(passport, LocalStrategy, User){
+
+  passport.serializeUser(function (user, done) {
+    var userID = user.dataValues.id;
+    console.log('serialized...');
+    return done(null, userID);
+  });
+
+  passport.deserializeUser(function (userID, done) {
+    User.findById(userID)
+    .then( (userFound) => {
+      if(userFound) {
+        console.log('deserialized...');
+        return done(null, userFound);
+      } else {
+        return done(null, false);
+      }
+    });
+  });
+
+  passport.use(new LocalStrategy(
+    function(user, pw, done) {
+      User.findOne({
+        where: { username : user, password: pw}
+      })
+      .then( (userFound) => {
+        if(userFound){
+          console.log('Found!');
+          return done(null, userFound);
+        } else {
+          console.log('Back to login.');
+          return done(null, false);
+        }
+      });
+    })
+  );
+
+};
