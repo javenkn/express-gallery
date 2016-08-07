94e72b38e4fb701c90c0c5eb33bae1a9ccd57bf7
javenkn <javenkn7@yahoo.com>
2016-08-06 14:09:51 -1000

    added faker to test seeder and package json

---
+++ seeders/20160806205924-test-photo-file.js
@@ -0,0 +1,25 @@
+'use strict';
+var faker = require('faker');
+var arrayOfPictures = [];
+
+for(var i = 0; i < 1; i++){
+  var picObj = {};
+  picObj.url = faker.image.imageUrl(700, 700, 'cats');
+  picObj.author = faker.name.firstName();
+  picObj.description = 'cats';
+  picObj.createdAt = new Date();
+  picObj.updatedAt = new Date();
+  arrayOfPictures.push(picObj);
+}
+
+console.log(arrayOfPictures);
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Photos', arrayOfPictures, {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Photos');
+  }
+};
